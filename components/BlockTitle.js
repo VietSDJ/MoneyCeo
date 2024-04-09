4e138932b6072c33a648c1c76234b2855b4099df
lhqbao2 <164971715+lhqbao2@users.noreply.github.com>
2024-04-09 17:45:59 +0700

    update padding

--- components/BlockTitle.js
+++ components/BlockTitle.js
@@ -3,8 +3,8 @@ import React from "react";
 const BlockTitle = (props) => {
   return (
     <div className={`block-title text-${props.textAlign} ${props.className}`}>
-      <p className="para-text text-custom-left">{props.paraText}</p>
-      <h3 className="fit-width text-custom-left">{props.titleText}</h3>
+      <p className="para-text ">{props.paraText}</p>
+      <h3 className="fit-width ">{props.titleText}</h3>
     </div>
   );
 };
